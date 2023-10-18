67ab18cbdc9eb8cbe2eca8eecd14a20e359f889f
Mohamed Abdullah <mm8909840@gmail.com>
2023-10-18 16:10:36 +0300

    Update File Bom.js

--- broadcast/bom.js
+++ broadcast/bom.js
@@ -88,17 +88,144 @@ document.body.appendChild(btn);
 
 
 // Example 3
-function sayHello() {
-    console.log(`Welcome`);
-}
+// function sayHello() {
+//     console.log(`Welcome`);
+// }
 
-let ele = document.getElementById("btn");
+// let ele = document.getElementById("btn");
 
-let counter = setTimeout(sayHello, 3000); //Return Count setTimeout
+// let counter = setTimeout(sayHello, 3000); //Return Count setTimeout
 
-ele.onclick = function () {
-    clearTimeout(counter);
-}
+// ele.onclick = function () {
+//     clearTimeout(counter);
+// }
+
+
+/******************************/
+
+/*
+BOM [Browser Object Model]
+    - setInterval(function , Millseconds , Additional Params) 
+    - clearInterval(Identifier) 
+*/
+
+// setInterval(() => {
+//     console.log("Msg");
+// }, 1000); // Prints the message every 1000 to infinity
+
+
+// Example 2
+// function printName(user, age) {
+//     console.log(`The Name ${user} Is a Age ${age}`);
+// }
+
+// setInterval(printName, 1000 , "Mohamed" , 26);
+
+
+// Example 3 CountDown  
+
+// let devInterval = document.createElement("div");
+// devInterval.id = "Interval";
+// let textDivInterval = document.createTextNode(5);
+
+// devInterval.appendChild(textDivInterval);
+
+// document.body.appendChild(devInterval);
+
+// function countDown() {
+//     devInterval.innerHTML -= 1;
+//     if (devInterval.innerHTML === "0") {
+//         clearInterval(clearInt)
+//     }
+// }
+
+// let clearInt = setInterval(countDown, 1000);
+
+
+
+/******************************/
+
+/*
+BOM [Browser Object Model]
+    - location object
+    --- href Get / Set [URL || Hash || File || Mail]
+    --- host
+    --- hash
+    --- protocol
+    --- reload()
+    --- replace()
+    --- assign()
+*/
+
+console.log(location);
+
+// href => not deleted search history
+// console.log(location.href); // get => http://localhost/BroadcastJavascript/
+// location.href = "https://google.com"; // set => Change Location
+
+// location.href = "https://developer.mozilla.org/en-US/docs/Web/JavaScript#advanced";
+
+// host
+// console.log(location.host); // broadcastjavascript.test + port
+
+// hostname
+// console.log(location.hostname); // broadcastjavascript.test
+
+// protocol
+console.log(location.protocol); // http:
+
+// hash
+console.log(location.hash); // get Hash In href 
+
+
+// reload()
+// console.log(location.reload()); // Reload The Page
+
+
+// replace() => Removes the current page from the session history and navigates to the given URL.
+// console.log(location.replace("https://google.com"));
+
+
+// assign() => Navigates to the given URL.
+// console.log(location.assign("https://google.com"));
+
+
+
+/******************************/
+
+/*
+BOM [Browser Object Model]
+    - open(URL [Opt] , Window Name Or Target Attr [Opt] , Win Features [Opt])
+    - close()
+    - Window Features
+    -- Width  [Num]
+    -- Height [Num]
+    -- Left   [Num]
+    -- Top    [Num]
+
+    Search
+    - Window.open Window Features
+*/
+
+
+// setTimeout(() => {
+//     window.open("https://google.com" , "_blank" , "width=400, height=400, left=400,top=500");
+// }, 2000);
+
+// setTimeout(() => {
+//     window.open("https://google.com" , "_self" , "width=400, height=400, left=400,top=500");
+// }, 2000);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
