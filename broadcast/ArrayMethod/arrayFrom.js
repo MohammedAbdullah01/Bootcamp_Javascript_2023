ce0ea57fb3a07e36d3d217ee0762f143c0988d76
Mohamed Abdullah <mm8909840@gmail.com>
2023-11-06 15:00:51 +0200

    Create Directory ArrayMethod

---
+++ broadcast/ArrayMethod/arrayFrom.js
@@ -0,0 +1,12 @@
+/*
+Array Method 
+- Array.from(Iterable , MapFunction , This)
+
+--- Variable
+--- String Numbers
+--- Set 
+--- Using The Map Function 
+--- Arrow Function 
+--- Shorten The Method + Use arguments
+*/
+
